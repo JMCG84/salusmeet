232948a4ed0df340ed0e6c6913668a87fa4dce67
Jose M Campos <camposgarciajosemanuel@gmail.com>
2026-02-16 16:47:58 +0100

    feat: setup vue router and home page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="min-h-screen bg-slate-50 flex items-center justify-center">
-    <h1 class="text-4xl font-bold text-slate-900">Hello, Salusmeet!</h1>
-  </div>
+  <router-view />
 </template>
 
 <style scoped></style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center">
+    <h1 class="text-2xl font-semibold">Home Salusmeet</h1>
+  </div>
+</template>
